3fb4bcb726301074b000b683ad5b7e0c561294e2
Setiawan Indra <zetiawanindra@gmail.com>
2024-01-23 10:19:04 +0700

    update

--- quotes/script.js
+++ quotes/script.js
@@ -1,19 +1,21 @@
-const quoteBtn = document.querySelector("button");
-const copyBtn = document.querySelector(".copy");
-const quoteText = document.querySelector(".quote");
-const authorName = document.querySelector(".author");
+const result = document.getElementById("result");
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
 const speech = document.querySelector(".speech");
+const copy = document.querySelector(".copy");
+const screenshoot = document.getElementById("screenshoot");
+
+const quoteBtn = document.querySelector("button");
+
+const quoteArea = document.querySelector(".quote-area");
+
+const apiKey = "aVXFTLU+zUdVIjNbsPUgbw==VBGKLy8lwxRlOVjf";
+let count = 0;
+
 synth = speechSynthesis;
 
 async function generateIdea() {
-  const result = document.getElementById("result");
-  const quote = document.getElementById("quote");
-  const author = document.getElementById("author");
-  const apiKey = "aVXFTLU+zUdVIjNbsPUgbw==VBGKLy8lwxRlOVjf";
-  const screenshoot = document.getElementById("screenshoot");
-  const icon = document.querySelector("icon");
-  let count = 0;
-
   quoteBtn.classList.add("loading");
   quoteBtn.innerText = "Loading Quote...";
 
@@ -24,10 +26,16 @@ async function generateIdea() {
     },
   };
 
+  // const url = "https://www.boredapi.com/api/activity";
+  // const response = await fetch(url);
+
   const url = "https://api.api-ninjas.com/v1/quotes?category=success";
   const response = await fetch(url, options);
 
   const api = await response.json();
+
+  // quote.textContent = api.activity;
+  // author.textContent = api.type;
 
   quote.textContent = api[0].quote;
   author.textContent = api[0].author;
@@ -38,18 +46,37 @@ async function generateIdea() {
   screenshoot.addEventListener("click", () => {
     count && location.reload();
 
+    screenshoot.removeAttribute("class", "");
+    screenshoot.setAttribute("class", "fas fa-download");
+
+    speech.style.display = "none";
+    copy.style.display = "none";
+    screenshoot.style.display = "none";
+    quoteBtn.style.display = "none";
+
     html2canvas(result).then((callback) => {
       screenshoot.setAttribute("href", callback.toDataURL("image/png"));
       screenshoot.setAttribute("download", "quote.png");
       count = 1;
+
+      quoteBtn.style.display = "block";
+      speech.style.display = "flex";
+      copy.style.display = "flex";
+      screenshoot.style.display = "flex";
     });
+  });
+
+  quoteBtn.addEventListener("click", () => {
+    screenshoot.setAttribute("class", "fa fa-camera");
+    screenshoot.removeAttribute("href");
+    count = 0;
   });
 }
 
 speech.addEventListener("click", () => {
   if (!quoteBtn.classList.contains("loading")) {
     let utterance = new SpeechSynthesisUtterance(
-      `${quoteText.innerText} by ${authorName.innerText}`
+      `${quote.innerText} by ${author.innerText}`
     );
     synth.speak(utterance);
     setInterval(() => {
@@ -60,6 +87,6 @@ speech.addEventListener("click", () => {
   }
 });
 
-copyBtn.addEventListener("click", () => {
-  navigator.clipboard.writeText(quoteText.innerText);
+copy.addEventListener("click", () => {
+  navigator.clipboard.writeText(quote.innerText);
 });
